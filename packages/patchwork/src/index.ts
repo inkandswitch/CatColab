0f07d91886f1fa3a5028bcc672f0e340d7bc4383
Paul Sonnentag <paul.sonnentag@googlemail.com>
2025-07-29 09:55:11 +0200

    only export side by side

--- packages/patchwork/src/index.ts
+++ packages/patchwork/src/index.ts
@@ -26,38 +26,38 @@ export const plugins: Plugin<any>[] = [
         },
         unlisted: true,
     },
-    {
-        type: "patchwork:tool",
-        id: "catcolab-model-view",
-        name: "Model",
-        icon: "Zap",
-        supportedDataTypes: ["catcolab-model"],
-        async load() {
-            const { ModelTool } = await import("./tools");
+    // {
+    //     type: "patchwork:tool",
+    //     id: "catcolab-model-view",
+    //     name: "Model",
+    //     icon: "Zap",
+    //     supportedDataTypes: ["catcolab-model"],
+    //     async load() {
+    //         const { ModelTool } = await import("./tools");
 
-            return {
-                EditorComponent: ModelTool,
-            };
-        },
-    },
-    {
-        type: "patchwork:tool",
-        id: "catcolab-model-analysis-view",
-        name: "Analysis",
-        icon: "Zap",
-        supportedDataTypes: ["catcolab-model"],
-        async load() {
-            const { AnalysisTool } = await import("./tools");
+    //         return {
+    //             EditorComponent: ModelTool,
+    //         };
+    //     },
+    // },
+    // {
+    //     type: "patchwork:tool",
+    //     id: "catcolab-model-analysis-view",
+    //     name: "Analysis",
+    //     icon: "Zap",
+    //     supportedDataTypes: ["catcolab-model"],
+    //     async load() {
+    //         const { AnalysisTool } = await import("./tools");
 
-            return {
-                EditorComponent: AnalysisTool,
-            };
-        },
-    },
+    //         return {
+    //             EditorComponent: AnalysisTool,
+    //         };
+    //     },
+    // },
     {
         type: "patchwork:tool",
         id: "catcolab-model-side-by-side-view",
-        name: "Side by Side",
+        name: "CatColab",
         icon: "Zap",
         supportedDataTypes: ["catcolab-model"],
         async load() {
